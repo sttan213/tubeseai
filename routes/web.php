e7051e1cd4dcc7d51cf8b5c55d986e322f9590e1
sttan213 <stanialfh213@gmail.com>
2022-06-20 16:51:09 +0700

    api baru

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,25 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('home');
+});
+
+Route::get('datapemasanganalat', function () {
+    return view('pasangalat');
+});
+
+Route::get('datamaintenance', function () {
+    return view('maintenance');
+});
+
+Route::get('dataticketing', function () {
+    return view('ticketing');
+});
+
+Route::get('dataketersediaan', function () {
+    return view('ketersediaan');
+});
+
+Route::get('datapembelian', function () {
+    return view('pembelian');
 });
