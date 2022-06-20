b0210ea5b13c339ea6044497fe8690f692cfe678
sttan213 <stanialfh213@gmail.com>
2022-06-20 13:03:34 +0700

    bisa

--- routes/web.php
+++ routes/web.php
@@ -14,25 +14,5 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('home');
-});
-
-Route::get('datapemasanganalat', function () {
-    return view('pasangalat');
-});
-
-Route::get('datamaintenance', function () {
-    return view('maintenance');
-});
-
-Route::get('dataticketing', function () {
-    return view('ticketing');
-});
-
-Route::get('dataketersediaan', function () {
-    return view('ketersediaan');
-});
-
-Route::get('datapembelian', function () {
-    return view('pembelian');
+    return view('welcome');
 });
